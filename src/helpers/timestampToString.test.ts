b924e7615f2908e5933b7a7258a3f4c7e8ae4195
Rafaelkocharli <Rafaelkocharli@gmail.com>
2024-07-28 19:29:45 +0300

    added eslint

--- src/helpers/timestampToString.test.ts
+++ src/helpers/timestampToString.test.ts
@@ -3,24 +3,21 @@ import { timestampToString } from "./timestampToString";
 describe("timestampToString function positive testing", () => {
   test("number up to day", () => {
     expect(timestampToString(1722099345, 1722099355)).toBe("Только что");
-    expect(timestampToString(1722098765, 1722099355)).toBe("9 м. назад");
-    expect(timestampToString(1722095755, 1722099355)).toBe("1 ч. назад");
+    expect(timestampToString(1722098765, 1722099355)).toBe("10 м. назад");
     expect(timestampToString(1722059255, 1722099355)).toBe("11 ч. назад");
-    expect(timestampToString(1722014455, 1722099355)).toBe("23 ч. назад");
     expect(timestampToString(1722014455, 1722100855)).toBe("Вчера");
   });
   test("number from day to year", () => {
-    expect(timestampToString(1721868255, 1722100855)).toBe("2 д. назад");
-    expect(timestampToString(1721499855, 1722100855)).toBe("6 д. назад");
+    expect(timestampToString(1721868255, 1722100855)).toBe("3 д. назад");
+    expect(timestampToString(1721499855, 1722100855)).toBe("1 нед. назад");
     expect(timestampToString(1720899855, 1722100855)).toBe("2 нед. назад");
     expect(timestampToString(1719499855, 1722100855)).toBe("1 мес. назад");
   });
   test("number from 1 year", () => {
-    expect(timestampToString(1609459200, 1722100855)).toBe("3 г. назад");
-    expect(timestampToString(1577836800, 1722100855)).toBe("4 г. назад");
-    expect(timestampToString(1546300800, 1722100855)).toBe("5 лет назад");
-    expect(timestampToString(1514784000, 1722100855)).toBe("6 лет назад");
-    expect(timestampToString(1262304000, 1722100855)).toBe("14 лет назад");
+    expect(timestampToString(1609459200, 1722100855)).toBe("4 г. назад");
+    expect(timestampToString(1546300800, 1722100855)).toBe("6 лет назад");
+    expect(timestampToString(1514784000, 1722100855)).toBe("7 лет назад");
+    expect(timestampToString(1262304000, 1722100855)).toBe("15 лет назад");
   });
 });
 
--- src/helpers/timestampToString.ts
+++ src/helpers/timestampToString.ts
@@ -15,20 +15,20 @@ export function timestampToString(
   }
   if (diff < 60) {
     return "Только что";
-  } else if (diff < 3600) {
-    return `${Math.floor(diff / 60)} м. назад`;
-  } else if (diff < 86400) {
-    return `${Math.floor(diff / 3600)} ч. назад`;
+  } else if (diff < 3570) {
+    return `${Math.round(diff / 60)} м. назад`;
+  } else if (diff < 84600) {
+    return `${Math.round(diff / 3600)} ч. назад`;
   } else if (diff < 86400 * 2) {
     return "Вчера";
-  } else if (diff < 86400 * 7) {
-    return `${Math.floor(diff / 86400)} д. назад`;
-  } else if (diff < 86400 * 28) {
+  } else if (diff < 86400 * 6.5) {
+    return `${Math.round(diff / 86400)} д. назад`;
+  } else if (diff < 86400 * 27.5) {
     return `${Math.round(diff / (86400 * 7))} нед. назад`;
-  } else if (diff < 31556926) {
+  } else if (diff < 86400 * 364.5) {
     return `${Math.round(diff / (86400 * 28))} мес. назад`;
   } else {
-    const years = Math.floor(diff / 31556926);
+    const years = Math.round(diff / 31556926);
     if (
       [1, 2, 3, 4].includes(+years.toString().slice(-1)) &&
       (years < 10 || years > 20)
